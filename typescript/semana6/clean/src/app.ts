ae3b08c6365eb99c407c466ec43e744b768a2106
John Cevallos <joancemac@gmail.com>
2025-05-22 15:55:56 -0500

    Update project dependencies and enhance TypeScript configuration for improved database interaction
    - Added PostgreSQL, Reflect Metadata, and TypeORM dependencies to package.json.
    - Updated tsconfig.json to target ES2020 and include necessary libraries for decorators.
    - Refactored app.ts to initialize TypeORM and handle database connection errors.
    - Modified TodoRoutes to utilize a configurable datasource for improved flexibility.

--- typescript/semana6/clean/src/app.ts
+++ typescript/semana6/clean/src/app.ts
@@ -1,16 +1,22 @@
+import 'reflect-metadata';
 import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
-
-
-
+import { initializeTypeORM } from './data/typeorm/typeorm.config';
 
 (async()=> {
   main();
 })();
 
-
-function main() {
+async function main() {
+  // Inicializar TypeORM
+  try {
+    await initializeTypeORM();
+    console.log('Database initialized');
+  } catch (error) {
+    console.error('Error during database initialization:', error);
+    process.exit(1);
+  }
 
   const server = new Server({
     port: envs.PORT,
